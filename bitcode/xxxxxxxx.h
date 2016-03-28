ba122ba6b7cdf38fa201e455d925969161e22a27
guduxueyu <scjy@gengyuanfeng.local>
2016-03-28 15:30:36 +0800

    xxxxx
    xxxxx

---
+++ bitcode/xxxxxxxx.h
@@ -0,0 +1,13 @@
+//
+//  xxxxxxxx.h
+//  bitcode
+//
+//  Created by scjy on 16/3/28.
+//  Copyright © 2016年 耿远风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xxxxxxxx : UIViewController
+
+@end
